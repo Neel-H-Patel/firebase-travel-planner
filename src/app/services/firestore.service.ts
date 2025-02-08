5cd9b1cda4f66a9f78dfb8505d536da1d6028519
Neel Patel <neelpatel@smu.edu>
2025-02-08 16:31:44 -0600

    set up requests to single documents

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+
+// using firestore, you only import the functions that are needed
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  Firestore,
+  getDocFromServer,
+  setDoc,
+  updateDoc
+} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +19,43 @@ export class FirestoreService {
 
   constructor(
     private firestore: Firestore
-  ) {
+  ) {}
 
+  // requests to single documents
 
+  private async createDocument(docPath: string, data: any): Promise<void> {
+    const docRef = doc(this.firestore, docPath);
+    await setDoc(docRef, data);
   }
+
+  private async addDocument(collectionPath: string, data: any) {
+    const collectionRef = collection(this.firestore, collectionPath);
+    const doc = await addDoc(collectionRef, data);
+    return doc.id;
+  }
+
+  private async getDocument<T>(docPath: string): Promise<T | null> {
+    const docRef = doc(this.firestore, docPath);
+    const docSnap = await getDocFromServer(docRef);
+    if (docSnap.exists()) {
+      return docSnap.data() as T;
+    }
+    return null;
+  }
+
+  private async updateDocument(collectionPath: string, data: any): Promise<void> {
+    const docRef = doc(this.firestore, collectionPath);
+    await updateDoc(docRef, data);
+    // update Doc throws error if doc not found, below statement will create a new doc if doc not found
+    // await setDoc(docRef, data, { merge: true });
+  }
+
+  private async deleteDocument(docPath: string): Promise<void> {
+    const docRef = doc(this.firestore, docPath);
+    await deleteDoc(docRef);
+  }
+
+  // requests to collections
+
+  // requests to Collections Groups
 }
